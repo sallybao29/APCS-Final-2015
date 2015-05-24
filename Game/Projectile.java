24405aea13bfdd6ab18f51ce57c532fea4e1accf
sallybao29 <sallybao29@gmail.com>
2015-05-24 00:37:25 -0400

    edited projectile

--- Game/Projectile.java
+++ Game/Projectile.java
@@ -1,12 +1,29 @@
-public class Projectile{
-    private int range;
-    private String type;
-    private int damage;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
 
-    public Projectile(String t){
+public class Projectile{
+    private String type;
+    private String direction;
+
+    private int damage;
+    private int cost;
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+
+    private BufferedImage image;
+    private Rectangle bounds;
+
+    public Projectile(String t, String d){
+	direction = d;
+	String file = "../Sprites/Projectiles/";
 	type = t;
+
 	switch (t){
-	case "Book":
+	case "English":
+	    file += "English_";
 	    break;
 	case "History Textbook":
 	    break;
@@ -15,5 +32,53 @@ public class Projectile{
 	case "Trigonometry Textbook":
 	    break;
 	}
+	file += direction.substring(0, 1).toUpperCase();
+
+	image = null;
+
+	try {
+	    image = ImageIO.read(new File(file));
+	}
+	catch (Exception e){}
+
+	bounds = new Rectangle(x, y, image.getWidth(), image.getHeight());
+
+    }
+
+    public int getX(){
+	return x;
+    }
+
+    public int getY(){
+	return y;
+    }
+
+    public int getDamage(){
+	return damage;
+    }
+
+    public int getCost(){
+	return cost;
+    }
+
+    public Rectangle getBounds(){
+	return bounds;
+    }
+
+    public void setX(int x){
+	this.x = x;
+    }
+
+    public void setY(int y){
+	this.y = y;
+    }
+
+    public void update(){
+	x += dx;
+	y += dy;
+    }
+
+    public void draw(Graphics2D g){
+	g.drawImage(image, x, y, null);
     }
 }
